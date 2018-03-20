3f5f397ec1e5bcb7293ff1f2a9140be278a3f2ec
David Guzmán <davidgzmn@hotmail.com>
2018-03-20 09:16:37 -0700

    Modified the script, commenting in process

--- microtransactions.py
+++ microtransactions.py
@@ -1,206 +1,74 @@
-
-# coding: utf-8
-
-# # Project: Hypothesis Testing for Microtransactions
-# Brian is a Product Manager at FarmBurg, a company that makes a farming simulation social network game.  In the FarmBurg game, you can plow, plant, and harvest different crops.
-# 
-# Today, you will be acting as Brian's data analyst for an A/B Test that he has been conducting.
-
-# ## Part 1: Testing for Significant Difference
-
-# Start by importing the following modules that you'll need for this project:
-# - `pandas` as `pd`
-
-# In[22]:
-
-
+#Import the needed libraries and tests
 import pandas as pd
+from scipy.stats import chi2_contingency
+from scipy.stats import binom_test
 
+# We load the csv with click information for the adds
+#df = pd.read_csv('clicks.csv')
+df= pd.read_csv('/Users/david/Documents/GitHub/IntroToDataAnalysis/FarmBurgMicrotransactions/clicks.csv')
 
-# Brian tells you that he ran an A/B test with three different groups: A, B, and C.  You're kind of busy today, so you don't ask too many questions about the differences between A, B, and C.  Maybe they were shown three different versions of an ad.  Who cares?
-# 
-# (HINT: you will care later)
-# 
-# Brian gives you a CSV of results called `clicks.csv`.  It has the following columns:
-# - `user_id`: a unique id for each visitor to the FarmBerg site
-# - `ab_test_group`: either `A`, `B`, or `C` depending on which group the visitor was assigned to
-# - `click_day`: only filled in *if* the user clicked on a link to purchase
-# 
-# Load `clicks.csv` into the variable `df`.
-
-# In[23]:
-
-
-df = pd.read_csv('clicks.csv')
-
-
-# Define a new column called `is_purchase` which is `Purchase` if `click_day` is not `None` and `No Purchase` if `click_day` is `None`.  This will tell us if each visitor clicked on the Purchase link.
-
-# In[24]:
-
-
+# Create a new column that notes if the user_id completed a purchase when shown the ad
 df['is_purchased'] = df.click_day.apply(lambda x: 'Purchase' if pd.notnull(x) else 'no purchase')
-df.head(5)
+df.head(5) # Analize the df to verify that its made correctly
 
-
-# We want to count the number of users who made a purchase from each group.  Use `groupby` to count the number of `Purchase` and `No Purchase` from each `group`.  Save your answer to the variable `purchase_counts`.
-# 
-# **Hint**: Group by `group` and `is_purchase` and the function `count` on the column `user_id`.
-
-# In[38]:
-
-
+#Calculate total amount of purchases
 purchase_counts = df.groupby(['is_purchased','group']).user_id.count()
+#Calculate users who purchases by group A B and C
 groupA_purchases = df[ (df.is_purchased == 'Purchase') & (df.group=='A')].user_id.count()
 groupB_purchases = df[ (df.is_purchased == 'Purchase') & (df.group=='B')].user_id.count()
 groupC_purchases = df[ (df.is_purchased == 'Purchase') & (df.group=='C')].user_id.count()
-
+#Calculate users who didn't purchases by group A B and C
 groupA_no_purchase = df[(df.is_purchased == 'no purchase') & (df.group=='A')].user_id.count()
 groupB_no_purchase = df[(df.is_purchased == 'no purchase') & (df.group=='B')].user_id.count()
 groupC_no_purchase = df[(df.is_purchased == 'no purchase') & (df.group=='C')].user_id.count()
 
-print(groupA_purchases)
-print(groupB_purchases)
-print(groupC_purchases)
+#print the values to corroborate
+print("Group A purchases: {0}").format(groupA_purchases)
+print("Group B purchases: {0}").format(groupB_purchases)
+print("Group C purchases: {0}").format(groupC_purchases)
 
+print("Group A no purchase: {0}").format(groupA_no_purchase)
+print("Group B no purchase: {0}").format(groupB_no_purchase)
+print("Group C no purchase: {0}").format(groupC_no_purchase)
 
-print(groupA_no_purchase)
-print(groupB_no_purchase)
-print(groupC_no_purchase)
-
+# Create a contingency table to use in a Chi2 test
 contingency = [[groupA_purchases,groupA_no_purchase],[groupB_purchases,groupB_no_purchase],[groupC_purchases,groupC_no_purchase]]
-
-
-
-# This data is *categorical* and there are *more than 2* conditions, so we'll want to use a chi-squared test to see if there is a significant difference between the three conditions.
-# 
-# Start by filling in the contingency table below with the correct values:
-# ```py
-# contingency = [[groupA_purchases, groupA_not_purchases],
-#                [groupB_purchases, groupB_not_purchases],
-#                [groupC_purchases, groupC_not_purchases]]
-# ```
-
-# In[39]:
-
-
-from scipy.stats import chi2_contingency
-
-
-# Now import the function `chi2_contingency` from `scipy.stats` and perform the chi-squared test.
-# 
-# Recall that the *p-value* is the second output of `chi2_contingency`.
-
-# In[40]:
-
-
+# Do the Chi2 test to determine if there is a statistically significant difference between groups
 chi2, pval, dof, expected = chi2_contingency(contingency)
 
-
-# In[41]:
-
-
+#Print the p value, there is a difference, but where?
 print(pval)
 
-
-# Great! It looks like a significantly greater portion of users from Group A made a purchase.
-
-# ## Part 2: Testing for Exceeding a Goal
-# 
-# Your day is a little less busy than you expected, so you decide to ask Brian about his test.
-# 
-# **You**: Hey Brian! What was that test you were running anyway?
-# 
-# **Brian**: It was awesome! We are trying to get users to purchase a small FarmBurg upgrade package.  It's called a microtransaction.  We're not sure how much to charge for it, so we tested three different price points: \$0.99, \$1.99, and \$4.99.  It looks like significantly more people bought the upgrade package for \$0.99, so I guess that's what we'll charge.
-# 
-# **You**: Oh no! I should have asked you this before we did that chi-squared test.  I don't think that this was the right test at all.  It's true that more people wanted purchase the upgrade at \$0.99; you probably expected that.  What we really want to know is if each price point allows us to make enough money that we can exceed some target goal.  Brian, how much do you think it cost to build this feature?
-# 
-# **Brian**: Hmm.  I guess that we need to generate a minimum of $1000 per week in order to justify this project.
-# 
-# **You**: We have some work to do!
-
-# How many visitors came to the site this week?
-# 
-# Hint: Look at the length of `df`.
-
-# In[43]:
-
-
+#Get the complete amount of users
 weekly_customers = len(df)
 
-
-# Let's assume that this is how many visitors we generally get each week.  Given that, calculate the percent of visitors who would need to purchase the upgrade package at each price point (\$0.99, \$1.99, \$4.99) in order to generate \$1000 per week.
-
-# In[47]:
-
-
-# Calculate the number of people who would need to purchase a $0.99 upgrade in order to generate $1000.
-# Then divide by the number of people who visit the site each week.
+#Calculate how many people we need at each price point to get $1000 a week
+#which is the value we set as the worth-it point for using a certain ad
+#also determine the percent of total customers that amount would reprecent
 clients_at_99 = 1000/.99
 percent_at_99 = clients_at_99/weekly_customers
 
-
-# In[49]:
-
-
-# Calculate the number of people who would need to purchase a $1.99 upgrade in order to generate $1000.
-# Then divide by the number of people who visit the site each week.
 clients_at_1_99 = 1000/1.99
 percent_at_1_99 = clients_at_1_99/weekly_customers
 
-
-# In[50]:
-
-
-# Calculate the number of people who would need to purchase a $1.99 upgrade in order to generate $1000.
-# Then divide by the number of people who visit the site each week.
 clients_at_4_99 = 1000/4.99
 percent_at_4_99 = clients_at_4_99/weekly_customers
 
+#We use a binomial test determine if there is a difference between the results
+#and the needed values
 
-# Note that you need a smaller percentage of purchases for higher price points.
-# 
-# Now, for each group, perform a binomial test using `binom_test` from `scipy.stats`.
-# - `x` will be the number of purchases for that group
-# - `n` will be the total number of visitors assigned to that group
-# - `p` will be the target percent of purchases for that price point (calculated above)
-# 
-# Recall that:
-# - Group `A` is the \$0.99 price point
-# - Group `B` is the \$1.99 price point
-# - Group `C` is the \$4.99 price point
-
-# In[51]:
-
-
-# import the binomial test from scipy.stats here
-from scipy.stats import binom_test
-
-
-# In[53]:
-
-
-# Test group A here
+#we are determining if the amount of purchases we need per customer at price points
+#exceeds the percent of people who purchased at that given price point
 pvalA = binom_test(groupA_purchases, n=1666, p=percent_at_99)
 print(pvalA)
 
-
-# In[54]:
-
-
-# Test group B here
 pvalB = binom_test(groupB_purchases, n=(groupB_purchases+groupB_no_purchase), p=percent_at_1_99)
 print(pvalB)
 
-
-# In[55]:
-
-
-# Test group C here
 pvalC = binom_test(groupC_purchases, n=(groupC_purchases+groupC_no_purchase), p=percent_at_4_99)
 print(pvalC)
 
+#Surpirsingly, even thougth the .99 pricepoint sells the most apps, the percentage of our customers
+#who buy the app at that price point is not enough to reach the needed revenue
 
-# If any of the groups passed the binomial test with $p < 0.05$, then we can be confident that enough people will buy the upgrade package at that price point to justify the feature.
-# 
-# Which price point should Brian go with?  Did this surprise you?
+#The 4.99 pricepoint is the only one where our purchase percentage matches our present results
